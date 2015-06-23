47e64576e73794a4a37572242ed82404d3bca556
fsahsen <fsahsen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-23 15:32:35 +0000

    Importing git repo
    git-svn-id: https://plugins.svn.wordpress.org/avectra-netforum-single-sign-on/trunk@1186269 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- src/NetAuth/Views/NetforumHelp.php
+++
@@ -1,2 +0,0 @@
-<?php
-namespace NetAuth\Views; use WP\Views\View; use WP\Views\Page; class NetforumHelp extends View { protected $fields = array(); public function __construct() { $this->spa10a68(); } private function spa10a68() { include_once Page::getTemplatesPath(__DIR__) . '/help.tpl'; } }
